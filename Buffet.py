de8ab55c6e4704e9d4f42327abee746f52fbac05
hallo2005 <billhallowangerjr@gmail.com>
2025-04-21 09:35:06 +0000

    Added list of Buffet menu

---
+++ Buffet.py
@@ -0,0 +1,12 @@
+# Buffet
+
+simple_foods = ("rice", "fufu", "goat soup", "soft drink", "Beer")
+for simple_food in simple_foods:
+    print(simple_food.title())
+
+
+print(simple_foods)
+
+simple_foods = ("ebba", "GB", "goat soup", "soft drink", "Beer")
+for simple_food in simple_foods:
+    print(simple_food.title())
